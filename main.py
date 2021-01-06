ef99b279814f24f4b736b75e165f028b84d535db
yukun yang <yukunyang@ucsb.edu>
2021-01-06 10:54:36 -0600

    cifar89.72 dropout13

--- main.py
+++ main.py
@@ -196,7 +196,7 @@ if __name__ == '__main__':
 
     # Check whether a GPU is available
     if torch.cuda.is_available():
-        device = 2#torch.device("cuda")
+        device = 3#torch.device("cuda")
         cuda.init()
         c_device = aboutCudaDevices()
         print(c_device.info())
